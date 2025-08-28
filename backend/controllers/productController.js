3c168737d4736009d9a564f33adc4101537b64a7
krishankraghuvanshi <krishank.raghuvanshi@gmail.com>
2025-08-28 21:16:55 +0530

    products Carousel and some other functionalities added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -161,6 +161,16 @@ const createProductReview = asyncHandler(async (req, res) => {
     }
 })
 
+//@desc Get top rated product
+//@routes Get /api/products/top
+//@access public 
+const getTopProducts = asyncHandler(async (req, res)=>{
+    const products = await Product.find({}).sort({rating:-1}).limit(3)
+
+    res.status(200).json(products)
+})
+
+
 export {
     getProducts, 
     getProductById, 
@@ -168,5 +178,6 @@ export {
     updateProduct, 
     deleteProduct,
     uploadProductImage,
-    createProductReview
+    createProductReview,
+    getTopProducts,
 }
